bfc66948d544fada27e144e29c04882ad09c7e90
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-03-23 23:56:54 -0700

    fixed a bug in the possiblePawnMoves method and started possibleMovesRook method

--- gameMethods/generateMoves.js
+++ gameMethods/generateMoves.js
@@ -5,10 +5,13 @@ var possibleMovesPawn = function(pieceInfo, board) {
     var moves = [];
     var coordinate = pieceInfo.charAt(2) + pieceInfo.charAt(3);
     var arrC = common.calculatePiecePositionInArray(coordinate);
-    if(board[arrC[0] + 1][arrC[1]].getPiece() == "") {
+    if( pieceInfo.charAt(0) == 'W' && board[arrC[0] - 1][arrC[1]].getPiece() != "") {
         return moves;
     }
-    if(pieceInfo.charAt(2) == 'G' && pieceInfo.charAt(0) == 'W') {
+    if(pieceInfo.charAt(0) == 'B' && board[arrC[0] + 1][arrC[1]].getPiece() != "") {
+        return moves;
+    }
+    if(pieceInfo.charAt(2) == 'G' && pieceInfo.charAt(0) == 'W' && board[arrC[0] - 2][arrC[1]].getPiece() == "") {
         moves.push(String.fromCharCode(pieceInfo.charCodeAt(2)-2) + pieceInfo.charAt(3));
     }
     if(pieceInfo.charAt(2) == 'B' && pieceInfo.charAt(0) == 'B') {
@@ -23,9 +26,18 @@ var possibleMovesPawn = function(pieceInfo, board) {
     return moves;
 }
 
+//gives the possible moves for a rook
+var possibleMovesRook = function(pieceInfo, board) {
+    var coordinate = pieceInfo.charAt(2) + pieceInfo.charAt(3);
+    var currentPos = common.calculatePiecePositionInArray(coordinate);
+    while(board[currentPos[0]][currentPos[1]+1].getPiece()) {
+        
+    }
+}
+
 var w = common.createBoard(8);
 var x = common.assignCoordinates(w);
-var fh = "BPB7";
+var fh = "WPG7";
 var b = common.createBoard(8);
 var cb = common.assignCoordinates(b);
 var y = possibleMovesPawn(fh, cb);
