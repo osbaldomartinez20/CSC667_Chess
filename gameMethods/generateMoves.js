427f9789ca37e57915d4c156076b4f6079cbab0f
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-03-28 14:05:56 -0700

    finished move generator function for bishop.

--- gameMethods/generateMoves.js
+++ gameMethods/generateMoves.js
@@ -34,24 +34,24 @@ var possibleMovesRook = function(pieceInfo, board) {
     var tempC = coordinate;
     const currentPos = common.calculatePiecePositionInArray(coordinate);
     var tempP = [currentPos[0], currentPos[1]];
-    while(tempP[1] + 1 != 8  && board[tempP[0]][tempP[1]+1].getPiece() == "") {
+    while(tempP[1] + 1 < 8  && board[tempP[0]][tempP[1]+1].getPiece() == "") {
         moves.push(tempC.charAt(0) + (tempP[1] + 2));
         tempP[1]++;
     }
     tempP = [currentPos[0], currentPos[1]];
-    while(tempP[0] + 1 != 8 && board[tempP[0] + 1][tempP[1]].getPiece() == "") {
+    while(tempP[0] + 1 < 8 && board[tempP[0] + 1][tempP[1]].getPiece() == "") {
         moves.push(String.fromCharCode(tempC.charCodeAt(0)+1) + (tempC.charAt(1)));
         tempP[0]++;
         tempC = String.fromCharCode(tempC.charCodeAt(0)+1) + tempC.charAt(1);
     }
     tempP = [currentPos[0], currentPos[1]];
     tempC = coordinate;
-    while(tempP[1] - 1 != -1 && board[tempP[0]][tempP[1] - 1].getPiece() == "") {
+    while(tempP[1] - 1 > -1 && board[tempP[0]][tempP[1] - 1].getPiece() == "") {
         moves.push(tempC.charAt(0) + (tempP[1]));
         tempP[1]--;
     }
     tempP = [currentPos[0], currentPos[1]];
-    while(tempP[0] - 1 != -1 && board[tempP[0] - 1][tempP[1]].getPiece() == "") {
+    while(tempP[0] - 1 > -1 && board[tempP[0] - 1][tempP[1]].getPiece() == "") {
         moves.push(String.fromCharCode(tempC.charCodeAt(0)-1) + (tempC.charAt(1)));
         tempP[0]--;
         tempC = String.fromCharCode(tempC.charCodeAt(0)-1) + tempC.charAt(1);
@@ -59,6 +59,7 @@ var possibleMovesRook = function(pieceInfo, board) {
     return moves;
 }
 
+//gives the possible moves for a knight
 var possibleMovesKnight = function(pieceInfo, board) {
     var moves = [];
     const coordinate = pieceInfo.charAt(2) + pieceInfo.charAt(3);
@@ -97,16 +98,51 @@ var possibleMovesKnight = function(pieceInfo, board) {
             moves.push(String.fromCharCode(tempC.charCodeAt(0)-1) + String.fromCharCode(tempC.charCodeAt(1)-2));
         } 
     }
+    return moves;
+}
+
+var possibleMovesBishop = function(pieceInfo, board) {
+    var moves = [];
+    const coordinate = pieceInfo.charAt(2) + pieceInfo.charAt(3);
+    var tempC = coordinate;
+    const currentPos = common.calculatePiecePositionInArray(coordinate);
+    var tempP = [currentPos[0], currentPos[1]];
+    while((tempP[0] + 1 < 8 && tempP[1] + 1 < 8) && board[tempP[0]+1][tempP[1]+1].getPiece() == "") {
+        moves.push(String.fromCharCode(tempC.charCodeAt(0)+1) + String.fromCharCode(tempC.charCodeAt(1)+1));
+        tempP =[tempP[0] + 1, tempP[1] + 1];
+        tempC = String.fromCharCode(tempC.charCodeAt(0)+1) + String.fromCharCode(tempC.charCodeAt(1)+1);
+    }
+    tempP = [currentPos[0], currentPos[1]];
+    tempC = coordinate;
+    while((tempP[0] + 1 < 8 && tempP[1] - 1 > -1) && board[tempP[0]+1][tempP[1]-1].getPiece() == "") {
+        moves.push(String.fromCharCode(tempC.charCodeAt(0)+1) + String.fromCharCode(tempC.charCodeAt(1)-1));
+        tempP =[tempP[0] + 1, tempP[1] - 1];
+        tempC = String.fromCharCode(tempC.charCodeAt(0)+1) + String.fromCharCode(tempC.charCodeAt(1)-1);
+    }
+    tempP = [currentPos[0], currentPos[1]];
+    tempC = coordinate;
+    while((tempP[0] - 1 > -1 && tempP[1] + 1 < 8) && board[tempP[0]-1][tempP[1]+1].getPiece() == "") {
+        moves.push(String.fromCharCode(tempC.charCodeAt(0)-1) + String.fromCharCode(tempC.charCodeAt(1)+1));
+        tempP =[tempP[0] - 1, tempP[1] + 1];
+        tempC = String.fromCharCode(tempC.charCodeAt(0)-1) + String.fromCharCode(tempC.charCodeAt(1)+1);
+    }
+    tempP = [currentPos[0], currentPos[1]];
+    tempC = coordinate;
+    while((tempP[0] - 1 > -1 && tempP[1] - 1 > -1) && board[tempP[0]-1][tempP[1]-1].getPiece() == "") {
+        moves.push(String.fromCharCode(tempC.charCodeAt(0)-1) + String.fromCharCode(tempC.charCodeAt(1)-1));
+        tempP =[tempP[0] - 1, tempP[1] - 1];
+        tempC = String.fromCharCode(tempC.charCodeAt(0)-1) + String.fromCharCode(tempC.charCodeAt(1)-1);
+    }
 
     return moves;
 }
 
 var w = common.createBoard(8);
 var x = common.assignCoordinates(w);
-var fh = "WND5";
+var fh = "WBD5";
 var b = common.createBoard(8);
 var cb = common.assignCoordinates(b);
-var y = possibleMovesKnight(fh, cb);
+var y = possibleMovesBishop(fh, cb);
 var cc = fh.charAt(2) + fh.charAt(3);
 if(x[3][1].getPiece() == "") {
     var t = common.calculatePiecePositionInArray(cc);
