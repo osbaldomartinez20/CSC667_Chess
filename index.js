28e0682a489ac1122a41e6a73eb5e28dbcfc22aa
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-04-08 19:02:00 -0700

    added a reference to views as public

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
+//important.. this line creates a connection to use static files such as html saved in the
+//folder public
+app.use(express.static('./views'))
+
 app.get('/', (req, res) => {
-	//res.send('Hey')
-	res.sendFile('/views/index.html', {root:__dirname})
+    //res.send('Hey')
+    res.sendFile('/views/index.html', { root: __dirname })
 })
 
-app.listen(3000, ()=> console.log('Server is running on port 3000'))
+app.listen(3000, () => console.log('Server is running on port 3000'))
