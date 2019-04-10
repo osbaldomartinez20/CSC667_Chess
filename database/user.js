1baa1fc2f84fecbcebd18877923d264f6e9a38ca
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-04-09 19:56:14 -0700

    finished create user.

--- database/user.js
+++ database/user.js
@@ -2,3 +2,19 @@
 //can create and update users
 
 var db = require('../config/db.config');
+
+
+exports.createUser = function (id, email) {
+
+    var display_name = email.substring(0, email.indexOf('@'));
+    var sql = "INSERT INTO users (user_id, email, display_name, wins, losses, elo) VALUES ('" + id + "','" + email + "', '" + display_name + "', 0, 0, 1200)";
+
+    db.query(sql, function (err, result) {
+        if(err) {
+            console.log("failed to create user " + err);
+            return;
+        } else {
+            console.log("user created");
+        }
+    });
+}
