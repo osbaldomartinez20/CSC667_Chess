d7f682f15ec551301eeef6508d8f31810c76744c
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-04-11 01:57:34 -0700

    fully working login between back and front end

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const https = require('https');
 const db = require('../auth/db_config.js')
 const bodyParser = require("body-parser");
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client('180146750892-kdcobp9c18m1fkhl8oa4gha04rdgukni.apps.googleusercontent.com');
 
 //create a router for url request
 const router = express.Router()
@@ -12,23 +15,24 @@ router.use(bodyParser.urlencoded({
 router.use(bodyParser.json());
 
 router.post('/login', (request, response) => {
-    const id_token = request.query.id_token
-    verify(id_token)
-    const { OAuth2Client } = require('google-auth-library')
-    const client = new OAuth2Client('80146750892-vh2nftso2rsa1h09ogk22qdd76ackhjh.apps.googleusercontent.com');
-
-    async function verify(id_token) {
+    var token = request.body.idtoken
+    console.log("the id token is " + token)
+        // verify(id_token)
+    async function verify() {
         const ticket = await client.verifyIdToken({
-            idToken: id_token,
-            audience: '80146750892-vh2nftso2rsa1h09ogk22qdd76ackhjh.apps.googleusercontent.com',
+            idToken: token,
+            audience: '80146750892-kdcobp9c18m1fkhl8oa4gha04rdgukni.apps.googleusercontent.com',
         })
         const payload = ticket.getPayload()
         const user_id = payload['sub']
+        const email = payload['email']
 
-        console.log(payLoad)
-        console.log(user_id)
+        console.log("here " + payload)
+        console.log("user_id " + user_id)
+        console.log("email " + email)
+        response.send(email)
     }
-    verify().catch(console.error)
+    verify().catch(console.error);
 })
 
 module.exports = router
