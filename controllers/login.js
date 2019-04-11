653fa279aea4f328739256a1d30093684ca6a15c
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-04-10 21:38:46 -0700

    create a router for backend/frontend communication

--- controllers/login.js
+++ controllers/login.js
@@ -1,16 +1,32 @@
 const db = require('../auth/db_config.js')
-const {OAuth2Client} = require('google-auth-library')
-const client = new OAuth2Client('80146750892-vh2nftso2rsa1h09ogk22qdd76ackhjh.apps.googleusercontent.com');
+const express = require('express')
+const bodyParser = require("body-parser");
 
-async function verify (id_token) {
-	const ticket = await client.verifyIdToken({
-		idToken: id_token,
-		audience: '80146750892-vh2nftso2rsa1h09ogk22qdd76ackhjh.apps.googleusercontent.com',
- 	})
-	const payload = ticket.getPayload()
-	const user_id = payload['sub']
-	
-	console.log(payLoad)
-	console.log(user_id)
-}
-verify().catch(console.error)
+//create a router for url request
+const router = express.Router()
+
+//to parse Json on receiven or request
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+router.use(bodyParser.json());
+
+router.get('/login', (request, response) => {
+    const id_token = request.query.id_token
+    verify(id_token)
+    const { OAuth2Client } = require('google-auth-library')
+    const client = new OAuth2Client('80146750892-vh2nftso2rsa1h09ogk22qdd76ackhjh.apps.googleusercontent.com');
+
+    async function verify(id_token) {
+        const ticket = await client.verifyIdToken({
+            idToken: id_token,
+            audience: '80146750892-vh2nftso2rsa1h09ogk22qdd76ackhjh.apps.googleusercontent.com',
+        })
+        const payload = ticket.getPayload()
+        const user_id = payload['sub']
+
+        console.log(payLoad)
+        console.log(user_id)
+    }
+    verify().catch(console.error)
+})
--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const express = require('express')
-const login =  require('./controllers/login.js')
 const bodyParser = require('body-parser')
 const app = express()
 
 //important.. this line creates a connection to use static files such as html saved in the
 //folder public
 app.use(express.static(__dirname + '/views'));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (request, response) => {
     response.sendFile('/views/index.html', { root: __dirname })
 })
 
-app.post('/login', (request, response) => { 
-    console.log('In login route')
-    login.verify(request.query.id_token)
-})
-
-app.get('/lobby', (request, response) => {
-    response.sendFile('/views/lobby.html', {root: __dirname })
-})
+const router = require('./controllers/login.js')
+app.use(router)
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
