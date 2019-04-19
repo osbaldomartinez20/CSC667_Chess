52c971fc1fe398f91eedc370088cfb9cfb752c9b
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-04-19 12:27:45 -0700

    finished routes for gamesTable.

--- Database/gamesTable.js
+++ Database/gamesTable.js
@@ -2,83 +2,64 @@
 var db = require('../auth/db_config.js');
 
 //create a new game by giving the username
-var createNewGame = async function (user) {
-    var playerid;
-    db.query("SELECT user_id FROM users WHERE display_name = '" + user + "'", function (err, result) {
+exports.createNewGame = async function (userid, callback) {
+    //this is used to asign an unique id to each game.
+    var time = new Date();
+    var game_id = time.getTime();
+    var sql = "INSERT INTO games (game_id, player_one_id, active, complete) VALUES (" + game_id + ", '" + userid + "', false , false)";
+    db.query(sql, function (err, result) {
         if (err) {
-            console.log("cannot retrieve ELO: " + err);
-            return false;
+            callback(err, null);
         } else {
-            playerid = JSON.parse(JSON.stringify(result))[0].user_id;
-            //this is used to asign an unique id to each game.
-            var time = new Date();
-            var game_id = time.getTime();
-            var sql = "INSERT INTO games (game_id, player_one_id, active, complete) VALUES (" + game_id + ", '" + playerid + "', false , false)";
-            db.query(sql, function (err, result) {
-                if (err) {
-                    console.log("failed to create game " + err);
-                    return false;
-                } else {
-                    console.log("game created");
-                }
-            });
+            callback(null, game_id);
         }
     });
 }
 
 //function used to join a game given an username and a game_id
-var joinGame = function (user, game_id) {
-    db.query("SELECT user_id FROM users WHERE display_name = '" + user + "'", function (err, result) {
+exports.joinGame = async function (userid, game_id, callback) {
+    var sql = "Update games SET player_two_id = '" + userid + "', active = true WHERE game_id = " + game_id + "";
+    db.query(sql, function (err, result) {
         if (err) {
-            console.log("cannot retrieve ELO: " + err);
-            return false;
+            callback(err, null);
         } else {
-            playerid = JSON.parse(JSON.stringify(result))[0].user_id;
-            var sql = "Update games SET player_two_id = '" + playerid + "', active = true WHERE game_id = " + game_id + "";
-            db.query(sql, function (err, result) {
-                if (err) {
-                    console.log("failed to join game " + err);
-                    return false;
-                } else {
-                    console.log("joined game");
-                }
-            });
+            callback(null, result);
         }
     });
 }
 
 //"class" used to organized the retrieved date when finding available games
-class availableData {
+exports.availableData = class {
     constructor(game_id, playerid) {
         this.game_id = game_id;
         this.playerid = playerid;
     }
 }
 
+//"class" used to organized the retrieved date when finding ongoing games
+exports.ongoingData = class {
+    constructor(game_id, playerid, player2id) {
+        this.game_id = game_id;
+        this.playerid = playerid;
+        this.player2id = player2id;
+    }
+}
+
 //helps finding available games
-var fetchAvailableGames = function (data, callback) {
+exports.fetchAvailableGames = function (callback) {
     db.query("SELECT game_id, player_one_id FROM games WHERE active = false AND complete = false", function (err, result) {
         if (err) {
             callback(err, null);
-        } else 
+        } else
             callback(null, result);
     });
 }
 
-//sends data with available games 
-var availableGames = function() {
-    var a_games = [];
-    var data = null;
-    fetchAvailableGames(data, function(err, result) {
+exports.fetchOngoingGames = function (callback) {
+    db.query("SELECT game_id, player_one_id, player_two_id FROM games WHERE active = true AND complete = false", function (err, result) {
         if (err) {
-            console.log("There was an error retrieving available games: " + err);
-            return false;
-        } else {
-            for (var i = 0; i < Object.keys(result).length; i++) {
-                const temp = new availableData(result[i].game_id, result[i].player_one_id);
-                a_games.push(temp);
-            }
-            console.log(JSON.stringify(a_games));
-        }
+            callback(err, null);
+        } else
+            callback(null, result);
     });
 }
