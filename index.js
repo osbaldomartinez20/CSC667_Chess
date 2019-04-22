3a24107f9f3ef130a1ffbed0dc5b18747415d4d7
nina mir <smirsaeidifarahani@mail.sfsu.edu>
2019-04-22 15:51:46 -0700

    seeking help from Anton connecting to backend!

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+//var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
 
 //important.. this line creates a connection to use static files such as html saved in the
 //folder public
+
+app.use(express.static(__dirname + '/node_modules'));
+
 app.use(express.static(__dirname + '/views'));
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (request, response) => {
     response.sendFile('/views/index.html', { root: __dirname })
+})
+
+app.get('/lobby', (request, response) => {
+    response.send('SUCCESS in JS!');
 })
 
 const router = require('./controllers/login.js')
