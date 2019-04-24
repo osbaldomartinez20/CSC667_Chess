d0cb3387eb1244516e76c6fb99e7a607c8454efa
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-04-24 02:14:40 -0700

    working chat feature

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-//var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
 
 //important.. this line creates a connection to use static files such as html saved in the
 //folder public
-
-app.use(express.static(__dirname + '/node_modules'));
 
 app.use(express.static(__dirname + '/views'));
 
@@ -20,13 +16,25 @@ app.get('/', (request, response) => {
     response.sendFile('/views/index.html', { root: __dirname })
 })
 
-app.get('/lobby', (request, response) => {
-    response.send('SUCCESS in JS!');
-})
+io.on('connection', function(socket) {
+    console.log('an user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+        io.emit('new message', ' *disconnected*');
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        console.log(msg);
+        io.emit('chat message', msg);
+    });
+});
+
 
 const router = require('./controllers/login.js')
 app.use(router)
 
-app.listen(3000, () => {
+http.listen(3000, () => {
     console.log("Server is up and listening on 3000...")
 })
