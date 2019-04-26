9eedd4865f7c2a45f583628bcc0109fa965bc043
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-04-25 18:48:25 -0700

    changed al the instances of searchig by display_name to user_id. I also added new methods in ranking.js to retrieve the top players by elo, to retrieve the elo of a player, and retrieve the prop of a player wining against another player.

--- Database/ranking.js
+++ Database/ranking.js
@@ -1,6 +1,9 @@
 //the K constant needed for the ELO calculation
 var K = 24;
 
+//variable for the number of top players to display
+var top = 20;
+
 //database
 var db = require('../auth/db_config.js');
 
@@ -13,18 +16,18 @@ var winProbability = function (rank1, rank2) {
 //calculates the ELO rating after each match
 //The parameters are the display names of the users
 //and the won parameter is a 1 if user1 won and a 0 if user2 won
-exports.EloRank = function (user1, user2, won) {
+exports.updateEloRank = function (user1, user2, won) {
 
     var rank1 = 0;
     var rank2 = 0;
-    db.query("SELECT * FROM users WHERE display_name = '" + user1 + "' OR display_name = '" + user2 + "'", function (err, result) {
+    db.query("SELECT * FROM users WHERE user_id = '" + user1 + "' OR user_id = '" + user2 + "'", function (err, result) {
         if (err) {
             console.log("cannot retrieve ELO: " + err);
             return false;
         } else {
             //get the rankings for both players
-            rank1 = JSON.parse(JSON.stringify(result))[1].elo;
-            rank2 = JSON.parse(JSON.stringify(result))[0].elo;
+            rank1 = result[1].elo;
+            rank2 = result[0].elo;
 
             //calculate probalitity of second player winning
             var player2 = winProbability(rank1, rank2);
@@ -38,10 +41,10 @@ exports.EloRank = function (user1, user2, won) {
                 var new2 = K * (0 - player2);
                 rank1 += new1;
                 rank2 += new2;
-                var win1 = JSON.parse(JSON.stringify(result))[1].wins;
+                var win1 = result[1].wins;
                 win1++;
                 //update player 1 in database
-                db.query("UPDATE users SET elo = " + rank1 + ", wins = " + win1 + " WHERE display_name = '" + user1 + "'", function (err, result) {
+                db.query("UPDATE users SET elo = " + rank1 + ", wins = " + win1 + " WHERE user_id = '" + user1 + "'", function (err, result) {
                     if (err) {
                         console.log("cannot update ELO or wins: " + err);
                         return false;
@@ -49,10 +52,10 @@ exports.EloRank = function (user1, user2, won) {
                         console.log(result.affectedRows + " record(s) updated");
                     }
                 });
-                var losses2 = JSON.parse(JSON.stringify(result))[0].losses;
+                var losses2 = result[0].losses;
                 losses2++;
                 //update player 2 in database
-                db.query("UPDATE users SET elo = " + rank2 + ", losses = " + losses2 + " WHERE display_name = '" + user2 + "'", function (err, result) {
+                db.query("UPDATE users SET elo = " + rank2 + ", losses = " + losses2 + " WHERE user_id = '" + user2 + "'", function (err, result) {
                     if (err) {
                         console.log("cannot update losses: " + err);
                         return false;
@@ -65,10 +68,10 @@ exports.EloRank = function (user1, user2, won) {
             else {
                 rank1 = rank1 + K * (0 - player1);
                 rank2 = rank2 + K * (1 - player2);
-                var win2 = JSON.parse(JSON.stringify(result))[0].wins;
+                var win2 = result[0].wins;
                 win2++;
                 //update player 2 in database
-                db.query("UPDATE users SET elo = " + rank2 + ", wins = " + win2 + " WHERE display_name = '" + user2 + "'", function (err, result) {
+                db.query("UPDATE users SET elo = " + rank2 + ", wins = " + win2 + " WHERE user_id = '" + user2 + "'", function (err, result) {
                     if (err) {
                         console.log("cannot update ELO or wins: " + err);
                         return false;
@@ -76,10 +79,10 @@ exports.EloRank = function (user1, user2, won) {
                         console.log(result.affectedRows + " record(s) updated");
                     }
                 });
-                var losses1 = JSON.parse(JSON.stringify(result))[1].losses;
+                var losses1 = result[1].losses;
                 losses1++;
                 //update player 1 in database
-                db.query("UPDATE users SET elo = " + rank1 + ", losses = " + losses1 + " WHERE display_name = '" + user1 + "'", function (err, result) {
+                db.query("UPDATE users SET elo = " + rank1 + ", losses = " + losses1 + " WHERE user_id = '" + user1 + "'", function (err, result) {
                     if (err) {
                         console.log("cannot update ELO or losses: " + err);
                         return false;
@@ -94,8 +97,8 @@ exports.EloRank = function (user1, user2, won) {
 }
 
 //resets user stats back to  default numbers
-var resetToDefault = function (user) {
-    var sql = "UPDATE users SET elo = 1200, losses = 0, wins = 0 WHERE display_name = '" + user + "'";
+var resetToDefault = function (userid) {
+    var sql = "UPDATE users SET elo = 1200, losses = 0, wins = 0 WHERE user_id = " + userid + "";
     db.query(sql, function (err, result) {
         if (err) {
             console.log("cannot update ELO or losses: " + err);
@@ -106,4 +109,54 @@ var resetToDefault = function (user) {
     });
 }
 
-EloRank("ozo", "osbaldo", 0);
+//function that takes user_id as parameter and return ELO ranking
+exports.getElo = function (userid, callback) {
+    db.query("SELECT elo FROM users WHERE user_id = '" + userid + "'", function (err, result) {
+        if (err) {
+            callback(err, null);
+        } else {
+            console.log(result);
+            callback(null, result[0].elo);
+        }
+    });
+}
+
+//returns the probability of an user given 
+//@param 1 user_id
+//@param 2 user_id of opponent
+exports.getUserWinningProbability = function (u_id, o_id, callback) {
+    db.query("SELECT elo FROM users WHERE user_id = '" + o_id + "' OR user_id = '" + u_id + "'", function (err, result) {
+        if (err) {
+            console.log("Cannot get elo: " + err);
+            callback(err, null);
+        } else {
+            var p_win = winProbability(result[1].elo, result[0].elo);
+            callback(null, p_win);
+        }
+    });
+}
+
+//class to hold the display_name and elo ranking of a player
+var playerRankInfo = class {
+    constructor(display_name, elo) {
+        this.display_name = display_name;
+        this.elo = elo;
+    }
+}
+
+//returns a JSON with the top players and their ELO score
+exports.getTopPlayers = function (callback) {
+    var p_top = [];
+    var sql = "SELECT * FROM users ORDER BY elo DESC LIMIT " + top + "";
+    db.query(sql, function (err, result) {
+        if (err) {
+            console.log("Cannot retrieve data: " + err);
+            callback(err, null)
+        } else {
+            for (var i = 0; i < result.length; i++) {
+                p_top.push(new playerRankInfo(result[i].display_name, result[i].elo));
+            }
+            callback(null,JSON.stringify(p_top));
+        }
+    });
+}
