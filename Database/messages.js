8c48591e543ad84a1d13b4e16098c0b2537edc15
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-05-06 15:54:58 -0700

    fixed storeMesages.

--- Database/messages.js
+++ Database/messages.js
@@ -1,38 +1,9 @@
 //database
 var db = require('../auth/db_config.js');
 
-//this creates a new entry in the chat table
-exports.newMessageTracker = function (game_id) {
-    var sql = "INSERT INTO chat (chat_id) VALUES (" + game_id + ")";
-    db.query(sql, function (err, result) {
-        if (err) {
-            console.log("Failed to make a message tracker for game: " + game_id + ": " + err);
-        } else {
-            console.log("Can track messages of game: " + game_id);
-        }
-    });
-}
-
-var messageStructure = class {
-    constructor(user_name, message, timestamp) {
-        this.user_name = user_name;
-        this.message = message;
-        this.timestamp = timestamp;
-    }
-}
-
-var dummyData = class {
-    constructor(user_name, message, game_id) {
-        this.user_id = user_name;
-        this.message = message;
-        this.game_id = game_id;
-    }
-}
-
-
 //this method stores messages in the database given game_id, message, and user_id
 exports.storeMessage = function (data) {
-    var sql = "INSERT INTO chat (chat_id, messages, user_id) VALUES (" + data.chat_id + ", '" + data.message + "', '" + data.user_id + "')";
+    var sql = "INSERT INTO chat (chat_id, messages, user_id) VALUES ('" + data.chat_id + "', '" + data.message + "', '" + data.user_id + "')";
     db.query(sql, function (err, result) {
         if (err) {
             console.log("Cannot store message: " + err)
