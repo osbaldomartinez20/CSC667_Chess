d2211222b6082cb80a4415f993ea72e211b3d2a8
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-05-11 03:31:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.get('/', (request, response) => {
 
 var nsp = io.of('/default').clients();
 
-nsp.on('connection', function(socket) {
-            console.log('an user connected');
+//nsp.on('connection', function(socket) {
+//          console.log('an user connected');
 
-            /*   function updateUserNames() {
+/*   function updateUserNames() {
         nsp.emit('usernames', lobby_users);
     }
     nsp.on('connection', function(socket) {
@@ -59,61 +59,61 @@ nsp.on('connection', function(socket) {
 
 
 
-            nsp.on('connection', function(socket) {
-                socket.on('message', function(msg) {
-                    console.log(msg);
-                    message.storeMessage(msg);
-                    nsp.emit('message', msg);
-                });
-            })
+nsp.on('connection', function(socket) {
+    socket.on('message', function(msg) {
+        console.log(msg);
+        message.storeMessage(msg);
+        nsp.emit('message', msg);
+    });
+})
 
-            nsp.on('connection', function(socket) {
-                socket.on('newGame', function(msg) {
-                    console.log(msg);
-                    nsp.emit('newGame', msg);
-                });
-            });
+nsp.on('connection', function(socket) {
+    socket.on('newGame', function(msg) {
+        console.log(msg);
+        nsp.emit('newGame', msg);
+    });
+});
 
-            socket.on('disconnect', function() {
-                console.log('user disconnected');
-            });
-            //var io = io.of('/private');
-            /*io.on('connection', function(socket) {
-                console.log('an user connected');
-                socket.on('room', function(room) {
-                    socket.join(room)
-                    rooms = room;
-                    console.log("room: " + room);
-                })
+socket.on('disconnect', function() {
+    console.log('user disconnected');
+});
+//var io = io.of('/private');
+/*io.on('connection', function(socket) {
+    console.log('an user connected');
+    socket.on('room', function(room) {
+        socket.join(room)
+        rooms = room;
+        console.log("room: " + room);
+    })
 
-            });*/
+});*/
 
-            io.on('connection', function(socket) {
-                var room = socket.handshake['query']['rooms'];
-                socket.join(room);
-                console.log('user joined room #' + room);
+io.on('connection', function(socket) {
+    var room = socket.handshake['query']['rooms'];
+    socket.join(room);
+    console.log('user joined room #' + room);
 
-                socket.on('disconnect', function() {
-                    socket.leave(room)
-                    console.log('user disconnected');
-                });
+    socket.on('disconnect', function() {
+        socket.leave(room)
+        console.log('user disconnected');
+    });
 
-                socket.on('message', function(msg) {
-                    console.log(msg);
-                    message.storeMessage(msg);
-                    io.to(room).emit('message', msg);
-                });
+    socket.on('message', function(msg) {
+        console.log(msg);
+        message.storeMessage(msg);
+        io.to(room).emit('message', msg);
+    });
 
-                socket.on('move', function(msg) {
-                    console.log(msg);
-                    moves.storeMove(msg);
-                    io.to(room).emit('move', msg);
-                });
-            });
+    socket.on('move', function(msg) {
+        console.log(msg);
+        moves.storeMove(msg);
+        io.to(room).emit('move', msg);
+    });
+});
 
-            const router = require('./controllers/login.js')
-            app.use(router)
+const router = require('./controllers/login.js')
+app.use(router)
 
-            http.listen(3000, () => {
-                console.log("Server is up and listening on 3000...")
-            })
+http.listen(3000, () => {
+    console.log("Server is up and listening on 3000...")
+})
